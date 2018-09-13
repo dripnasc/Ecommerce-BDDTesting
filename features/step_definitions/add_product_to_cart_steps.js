d16000c076a9af7e0d0bd618c5669d581ad2af65
dripnasc <dri.pnasc@gmail.com>
2018-09-13 00:54:30 -0300

    Fixing Add  Steps

--- features/step_definitions/add_product_to_cart_steps.js
+++ features/step_definitions/add_product_to_cart_steps.js
@@ -9,7 +9,7 @@ const { By, until, key } = require('selenium-webdriver');
 //Scenario Outline: Add <quantity> items of product to cart
 When('I select {int} of this product', async function (quantity) {
   const plusBtn = await this.driver.findElement(By.className('adds track-satellite-oneclick'));
-  for (click = 0; click < quantity; click++) {
+  for (click = 0; click < quantity-1; click++) {
     plusBtn.click();
   }
 });
@@ -17,4 +17,5 @@ When('I select {int} of this product', async function (quantity) {
 
 When('I click to add the product to my cart', async function () {
   await this.driver.findElement(By.className('button-success button-pill right buy-button buy-button-product fluid')).click();
+  await this.driver.sleep (10000);
 });
