6516434f2e7303e99e3163d201c283c447d13035
dripnasc <dri.pnasc@gmail.com>
2018-09-19 13:39:01 -0300

    Fixes

--- features/step_definitions/buy_products_steps.js
+++ features/step_definitions/buy_products_steps.js
@@ -19,20 +19,16 @@ When('I proceed to Check Out', async function () {
 When('I select a shipping address', async function () {
   await this.waitOverlay();
   await this.driver.findElement(By.css('body > div.wrapper > main > div > div > div.address-card.choose.view-address')).click();
-  //await this.driver.findElement(By.className('address-data')).click();
   await this.driver.findElement(By.css('[data-js="continue"]')).click();
 });
 
 When('I confirm product={string} and quantity={int} at my purchase summary', async function (productTitle, quantity) {
   await this.waitOverlay();
-  //confirming name
   await this.driver.findElement(By.className('product-list-item'));
   const summaryTitle = await this.driver.findElement(By.className('product-text')).getText();
   assert.strictEqual(summaryTitle, productTitle);
-  //confirming quantity
   const summaryQnt = await this.driver.findElement(By.className('product-item-quantity')).getText();
   assert.strictEqual(summaryQnt, quantity + 'x');
-  //Proceeding
   await this.driver.findElement(By.css('[data-js="continue"]')).click();
 });
 
